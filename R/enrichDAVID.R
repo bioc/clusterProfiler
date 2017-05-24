fa9125d2bc728fd6d74dc0d07b39399a10262792
Guangchuang Yu <guangchuangyu@gmail.com>
2017-05-24 05:46:07 +0000

    bug fixed
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_5/madman/Rpacks/clusterProfiler@129823 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/enrichDAVID.R
+++ R/enrichDAVID.R
@@ -58,30 +58,30 @@ enrichDAVID <- function(gene,
 
     ## addList will throw error if idType is not match.
     ## use match.arg to check before addList make it more readable
-    
+
     idType <- match.arg(idType, getIdTypes(david))
-    
+
     ##     getIdTypes(david)
-    ##  [1] "AFFYMETRIX_3PRIME_IVT_ID" "AFFYMETRIX_EXON_ID"      
-    ##  [3] "AGILENT_CHIP_ID"          "AGILENT_ID"              
-    ##  [5] "AGILENT_OLIGO_ID"         "APHIDBASE_ID"            
-    ##  [7] "BEEBASE_ID"               "BEETLEBASE_ID"           
-    ##  [9] "BGD_ID"                   "CGNC_ID"                 
-    ## [11] "CRYPTODB_ID"              "DICTYBASE_ID"            
-    ## [13] "ENSEMBL_GENE_ID"          "ENSEMBL_TRANSCRIPT_ID"   
-    ## [15] "ENTREZ_GENE_ID"           "FLYBASE_GENE_ID"         
-    ## [17] "GENBANK_ACCESSION"        "GENOMIC_GI_ACCESSION"    
-    ## [19] "GENPEPT_ACCESSION"        "LOCUS_TAG"               
-    ## [21] "MGI_ID"                   "MIRBASE_ID"              
-    ## [23] "MRNA_GI_ACCESSION"        "NASONIABASE_ID"          
-    ## [25] "PROTEIN_GI_ACCESSION"     "PSEUDOCAP_ID"            
-    ## [27] "REFSEQ_MRNA"              "REFSEQ_PROTEIN"          
-    ## [29] "RGD_ID"                   "SGD_ID"                  
-    ## [31] "TAIR_ID"                  "UNIGENE"                 
-    ## [33] "UNIPROT_ACCESSION"        "UNIPROT_ID"              
-    ## [35] "VECTORBASE_ID"            "WORMBASE_GENE_ID"        
+    ##  [1] "AFFYMETRIX_3PRIME_IVT_ID" "AFFYMETRIX_EXON_ID"
+    ##  [3] "AGILENT_CHIP_ID"          "AGILENT_ID"
+    ##  [5] "AGILENT_OLIGO_ID"         "APHIDBASE_ID"
+    ##  [7] "BEEBASE_ID"               "BEETLEBASE_ID"
+    ##  [9] "BGD_ID"                   "CGNC_ID"
+    ## [11] "CRYPTODB_ID"              "DICTYBASE_ID"
+    ## [13] "ENSEMBL_GENE_ID"          "ENSEMBL_TRANSCRIPT_ID"
+    ## [15] "ENTREZ_GENE_ID"           "FLYBASE_GENE_ID"
+    ## [17] "GENBANK_ACCESSION"        "GENOMIC_GI_ACCESSION"
+    ## [19] "GENPEPT_ACCESSION"        "LOCUS_TAG"
+    ## [21] "MGI_ID"                   "MIRBASE_ID"
+    ## [23] "MRNA_GI_ACCESSION"        "NASONIABASE_ID"
+    ## [25] "PROTEIN_GI_ACCESSION"     "PSEUDOCAP_ID"
+    ## [27] "REFSEQ_MRNA"              "REFSEQ_PROTEIN"
+    ## [29] "RGD_ID"                   "SGD_ID"
+    ## [31] "TAIR_ID"                  "UNIGENE"
+    ## [33] "UNIPROT_ACCESSION"        "UNIPROT_ID"
+    ## [35] "VECTORBASE_ID"            "WORMBASE_GENE_ID"
     ## [37] "XENBASE_ID"               "ZFIN_ID"
-    
+
     david.res <- addList(david, gene, idType=idType,
                          listName="clusterProfiler",
                          listType=listType)
@@ -148,7 +148,7 @@ enrichDAVID <- function(gene,
     ## gc <- strsplit(Over$geneID, "/")
     ## names(gc) <- Over$ID
 
-    if (!is.na(maxGSSize) || !is.null(maxGSSize)) {
+    if (!is.na(maxGSSize) && !is.null(maxGSSize)) {
         idx <- as.numeric(sub("/\\d+", "", Over$BgRatio)) <= maxGSSize
         Over <- Over[idx,]
     }
